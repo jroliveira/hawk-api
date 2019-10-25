4589406cc6f735290a357c815c4fbced99913f9b
Junior Oliveira <junolive@gmail.com>
2019-10-26 00:10:26 +0100

    fix: update readme and neo4j connection

--- src/Hawk.WebApi/Features/Account/NewAccountModelValidator.cs
+++ src/Hawk.WebApi/Features/Account/NewAccountModelValidator.cs
@@ -4,19 +4,8 @@
 
     internal sealed class NewAccountModelValidator : AbstractValidator<NewAccountModel>
     {
-        internal NewAccountModelValidator()
-        {
-            this.RuleFor(model => model.Email)
-                .NotEmpty()
-                .WithMessage("Account e-mail is required.");
-
-            this.RuleFor(model => model.Password)
-                .NotEmpty()
-                .WithMessage("Account password is required.");
-
-            this.RuleFor(model => model.ConfirmPassword)
-                .Equal(model => model.Password)
-                .WithMessage("Account confirm password must match account password.");
-        }
+        internal NewAccountModelValidator() => this.RuleFor(model => model.Email)
+            .NotEmpty()
+            .WithMessage("Account e-mail is required.");
     }
 }
