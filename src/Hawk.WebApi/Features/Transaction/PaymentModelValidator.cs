ce62ae98212b944339c6f150a174cecd56dff9a6
Junior Oliveira <junolive@gmail.com>
2020-02-23 18:00:50 +0000

    fix: transaction duplication when updating and inserting duplicate

--- src/Hawk.WebApi/Features/Transaction/PaymentModelValidator.cs
+++ src/Hawk.WebApi/Features/Transaction/PaymentModelValidator.cs
@@ -4,9 +4,18 @@
 
     using FluentValidation;
 
+    using Hawk.Domain.Currency.Queries;
+    using Hawk.Domain.PaymentMethod.Queries;
+    using Hawk.Domain.Shared;
+
+    using static Hawk.Domain.Shared.Queries.GetByIdParam<string>;
+
     internal sealed class PaymentModelValidator : AbstractValidator<PaymentModel>
     {
-        internal PaymentModelValidator()
+        internal PaymentModelValidator(
+            Email email,
+            IGetCurrencyByName getCurrencyByName,
+            IGetPaymentMethodByName getPaymentMethodByName)
         {
             this.RuleFor(model => model.Value)
                 .GreaterThan(0)
@@ -18,11 +27,15 @@
 
             this.RuleFor(model => model.Method)
                 .NotEmpty()
-                .WithMessage("Payment method is required.");
+                .WithMessage("Payment method is required.")
+                .MustAsync(async (method, _) => await getPaymentMethodByName.GetResult(NewGetByIdParam(email, method)))
+                .WithMessage("Payee not found.");
 
             this.RuleFor(model => model.Currency)
                 .NotEmpty()
-                .WithMessage("Payment currency is required.");
+                .WithMessage("Currency is required.")
+                .MustAsync(async (currency, _) => await getCurrencyByName.GetResult(NewGetByIdParam(email, currency)))
+                .WithMessage("Payee not found.");
         }
     }
 }
