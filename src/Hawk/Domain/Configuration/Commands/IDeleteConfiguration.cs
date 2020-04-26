1e8662089d7287909bcbb76349f6349bec8df11d
Junior Oliveira <junolive@gmail.com>
2020-04-26 17:18:00 +0100

    feat: delete configuration

---
+++ src/Hawk/Domain/Configuration/Commands/IDeleteConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace Hawk.Domain.Configuration.Commands
+{
+    using Hawk.Domain.Shared.Commands;
+
+    public interface IDeleteConfiguration : ICommand<DeleteParam<string>>
+    {
+    }
+}
