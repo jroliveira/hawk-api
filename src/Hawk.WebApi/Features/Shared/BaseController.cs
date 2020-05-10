78980e7b3438717a98dc93d5ee3c5df9d607fac7
Junior Oliveira <junolive@gmail.com>
2020-05-10 17:26:07 +0100

    fix: update transaction

--- src/Hawk.WebApi/Features/Shared/BaseController.cs
+++ src/Hawk.WebApi/Features/Shared/BaseController.cs
@@ -6,14 +6,20 @@
     using Microsoft.AspNetCore.Mvc;
 
     using static System.Net.WebUtility;
+    using static System.String;
 
     using static Hawk.WebApi.Infrastructure.ErrorHandling.ErrorHandler;
 
     public abstract class BaseController : ControllerBase
     {
-        internal IActionResult Created(object id, object value) => base.Created(
-            $"{this.Request.GetDisplayUrl()}/{UrlEncode(id.ToString())}",
-            value);
+        internal IActionResult Created(object id, object value)
+        {
+            var uri = this.Request.GetDisplayUrl().Contains(id?.ToString() ?? Empty)
+                ? $"{this.Request.GetDisplayUrl()}"
+                : $"{this.Request.GetDisplayUrl()}/{UrlEncode(id?.ToString())}";
+
+            return base.Created(uri, value);
+        }
 
         internal new IActionResult NoContent() => base.NoContent();
 
